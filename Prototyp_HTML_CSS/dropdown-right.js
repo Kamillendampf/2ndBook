acb393d205592dafb60b53a1a127e342582f46e6
lela1997 <daniela_ege@web.de>
2021-10-31 16:11:25 +0100

    LAYOUT UPDATE

--- Prototyp_HTML_CSS/dropdown-right.js
+++ Prototyp_HTML_CSS/dropdown-right.js
@@ -1,24 +1,3 @@
-
-
-function myFunction() {
-               
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(myFunction) {
-if (!event.target.matches('.icon-th-list')) {
-var dropdowns = document.getElementsByClassName("sidenav");
-var j;
-for (j = 0; j < dropdowns.length; j++) {
-var openDropdown = dropdowns[j];
-if (openDropdown.classList.contains('show')) {
-openDropdown.classList.remove('show');
-}
-}
-}
-}
-
 function myFunctionright() {
   document.getElementById("myDropdownright").classList.toggle("showright");
   
@@ -37,7 +16,3 @@ openDropdown.classList.remove('showright');
 }
 }
 } 
-
-
-  
-  
