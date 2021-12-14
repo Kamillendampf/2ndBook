93e6c264c4125860e609658de6aa6ec94217a428
MrJassy <deanbasic@web.de>
2021-12-15 00:16:36 +0100

    update

--- Prototyp_HTML_CSS/kontodaten.js
+++ Prototyp_HTML_CSS/kontodaten.js
@@ -38,23 +38,127 @@ function test1234() {
 }
 function test12345() {
     const host="http://localhost:8000/wba2api/Benutzer/gib/1";
+    var id;
+    var benutzername;
+    var passwort;
+    var vorname;
+    var nachname;
+    var strasse;
+    var hausnummer;
+    var plz;
+    var ort;
+    var email;
+    var wallet;
     const xhr = new XMLHttpRequest();
-    var namex=" ";
-    xhr.open('GET', host);
-    
-    // set response format
-    xhr.responseType = 'json';
-    xhr.send();
-    const user = xhr.response;
-    
-    // log details
-    console.log(user.benutzername); 
+    xhr.open('GET', host, false);
 
-   /* xhr.onload = () => {
-        // get JSON response
-        const user = xhr.responseText;
     
-        // log details
-        console.log(user); 
-    };*/
+    xhr.onload = function () {
+        //Request finished. Do processing here.
+        var user;
+        // get JSON response
+        user = xhr.response;
+        var json = JSON.stringify(user);
+        var user1 = JSON.parse(user);
+        //Daten in variablen sichern
+        id = user1.daten.id;
+        benutzername = user1.daten.benutzername;
+        passwort = user1.daten.passwort;
+        vorname = user1.daten.vorname;
+        nachname = user1.daten.nachname;
+        strasse = user1.daten.strasse;
+        hausnummer = user1.daten.hausnummer;
+        plz = user1.daten.plz;
+        ort = user1.daten.ort;
+        email = user1.daten.email;
+        wallet = user1.daten.wallet;
+
+        
+
+    };
+    xhr.send(null);
+    //einzelne Html-Felder ansprechen
+    document.getElementById("vorname").value = vorname;
+    document.getElementById("nachname").value = nachname;
+    document.getElementById("benutzername").value = benutzername;
+    document.getElementById("emailadresse").value = email;
+    document.getElementById("strasse").value = strasse;
+    document.getElementById("hausnummer").value = hausnummer;
+    document.getElementById("plz").value = plz;
+    document.getElementById("stadt").value = ort;
+    //document.getElementById("handynummer").value = hand
+    document.getElementById("wallet").value = wallet;
+}
+
+
+function test1() {
+    const host="http://localhost:8000/wba2api/Benutzer";
+   
+    var vorname = document.getElementById("vorname").value;
+    var nachname = document.getElementById("nachname").value;
+    var benutzername = document.getElementById("benutzername").value;
+    var emailadresse = document.getElementById("emailadresse").value;
+    var strasse = document.getElementById("strasse").value;
+    var hausnummer = document.getElementById("hausnummer").value;
+    var plz = document.getElementById("plz").value;
+    var ort = document.getElementById("stadt").value;
+    var passwort;// = document.getElementById("passwort").value;
+    //document.getElementById("handynummer").value = hand
+    var wallet = document.getElementById("wallet").value;
+    //xhr.open('PUT', host, false);
+    console.log(plz);
+        //wird geprüft, ob ein Value vorhanden ist oder nicht. 
+    if (vorname == ""){
+        window.alert("Du musst deinen Vorname angeben."); //wenn das Feld Vorname nicht ausgefüllt ist, wird die Medung ausgegeben, dass ein Vorname eingegeben werden muss.
+    }
+    else if  (nachname == ""){
+        window.alert("Du musst deinen Nachname angeben.");
+    }
+    else if  (benutzername == ""){
+        window.alert("Du musst einen Benutzername angeben.");
+    }
+    else if  (emailadresse == ""){
+        window.alert("Du musst deinen E-Mail angeben.");
+    }
+    else if  (handynummer == ""){
+        window.alert("Du musst deine Handynummer angeben.");
+    }
+    else if  (strasse == ""){
+        window.alert("Du musst deine Straße angeben.");
+    }
+    else if  (hausnummer == ""){
+        window.alert("Du musst deine Hausummer angeben.");
+    }
+    else if  (plz == ""){
+        window.alert("Du musst deine PLZ angeben.");
+    }
+    else if  (ort == ""){
+        window.alert("Du musst dein Ort angeben.");
+    }
+    else if  (passwort == ""){
+        window.alert("Du musst ein Passwort angeben.");
+    }
+    else{ 
+        var data = {       // wenn alle Felder befüllt sind, wird Liste erstellt
+        vorname: vorname,       //der erste Wert ist der KEY, zweiter die Variable die wir oben definiert haben.
+        nachname: nachname,
+        benutzername: benutzername,
+        email: emailadresse,
+        handynummer: handynummer,
+        strasse: strasse,
+        hausnummer: hausnummer,
+        plz: plz,
+        ort: ort,
+        passwort: "passwort"
+        
+    };
+    
+    var json = JSON.stringify(data);  //Liste wird umgewandelt in jason datei (strukturierte Datei)
+    console.log(json);
+    var xhr =  new XMLHttpRequest(); // //brauchen wir um an den Server zu senden
+    xhr.open("PUT", host);  //wollen was an Server senden  mit POST Methode, weil wir was AN DEN SERVER schicken wollen     
+    xhr.send(json); //json datei wird gesendet //an localhost..8000
+    //xhr.onload = function () {
+    //};
+}
 }
